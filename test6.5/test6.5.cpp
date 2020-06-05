d89e0518b4ff232aad1f33d2aa6e9acefa027fd5
Cube <877211593@qq.com>
2020-06-05 14:49:15 +0800

    wu

--- test6.5/test6.5.cpp
+++ test6.5/test6.5.cpp
@@ -2,10 +2,51 @@
 //
 
 #include <iostream>
+#include<opencv.hpp>
+#include <cmath>
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat src = imread("test.jpg");
+	Mat mask = Mat::zeros(src.size(), CV_8UC1);
+
+	if (src.empty())
+	{
+		cout << "找不到图片！" << endl;
+		return 0;
+	}
+
+	int width = src.cols;
+	int height = src.rows;
+
+	int pixNum = width * height;
+	int clusterCount = 2;
+	Mat labels;
+	Mat centers;
+
+	//制作kmeans用的数据
+	Mat sampleData = src.reshape(3, pixNum);
+	Mat km_data;
+	sampleData.convertTo(km_data, CV_32F);
+
+	//执行kmeans
+	TermCriteria criteria = TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 0.1);
+	kmeans(km_data, clusterCount, labels, criteria, clusterCount, KMEANS_PP_CENTERS, centers);
+
+	//制作mask
+	uchar fg[2] = { 0,255 };
+	for (int row = 0; row < height; row++) {
+		for (int col = 0; col < width; col++) {
+			mask.at<uchar>(row, col) = fg[labels.at<int>(row*width + col)];
+		}
+	}
+	imshow("src", src);
+	imshow("mask", mask);
+	waitKey(0);
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
